1e825c5bde67cdc39e00869500822803dfa18970
HSJ <hesuijin@qq.com>
2021-03-08 20:19:24 +0800

    项目 用于jenkins测试自动化部署

---
+++ src/main/java/com/example/jenkins/demo/Jenkins.java
@@ -0,0 +1,20 @@
+package com.example.jenkins.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author HeSuiJin
+ * @Date 2021/3/8 20:08
+ * @Description:
+ */
+@RestController
+@RequestMapping("/jenkins")
+public class Jenkins {
+
+    @GetMapping("demo")
+    public String jenkinsDemo(){
+        return "hello jenkins";
+    }
+}
